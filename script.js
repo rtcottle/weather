637cc2b12fa8f67ac236bbe1a41cf5161db6a469
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-21 15:03:10 -0600

    local storage set works

--- script.js
+++ script.js
@@ -7,31 +7,33 @@ TODO: Display days 2-6
 TODO: Retain search as selectable button
 */
 
-var searchBtn = getElementByID("search");
+var searchBtn = document.getElementById("search");
 var API_KEY = "7b5f03654b5a109a294b890d0283e4e0";
-var cardContainer = getElementByID("card-container");
+var cardContainer = document.getElementById("card-container");
+var citySearch = document.getElementById("location");
 
 function getApi() {
-  var requestWeather = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${API_KEY}`;
+  localStorage.setItem("city", citySearch.value);
 
-  fetch(requestWeather)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {}
-    });
-  //these might go under the getApi function after the for loop.
-  var weatherHeader = document.createElement("header");
-  var fiveDayForecast = document.createElement("h3");
-  var weatherContainer = document.createElement("div");
-  var dayCard = document.createElement("span");
-  cardContainer.appendChild(weatherHeader);
-  cardContainer.appendChild(fiveDayForecast);
-  cardContainer.appendChild(weatherContainer);
-  cardContainer.appendChild(dayCard);
-  weatherHeader.setAttribute("class", "border border-dark");
-  dayCard.setAttribute("class", "bg-dark text-light");
+  //   var requestWeather = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${API_KEY}`;
+  //   fetch(requestWeather)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       for (var i = 0; i < data.length; i++) {}
+  //     });
+  //   //these might go under the getApi function after the for loop.
+  //   var weatherHeader = document.createElement("header");
+  //   var fiveDayForecast = document.createElement("h3");
+  //   var weatherContainer = document.createElement("div");
+  //   var dayCard = document.createElement("span");
+  //   cardContainer.appendChild(weatherHeader);
+  //   cardContainer.appendChild(fiveDayForecast);
+  //   cardContainer.appendChild(weatherContainer);
+  //   cardContainer.appendChild(dayCard);
+  //   weatherHeader.setAttribute("class", "border border-dark");
+  //   dayCard.setAttribute("class", "bg-dark text-light");
 }
 
-searchBtn.on("click", getApi);
+searchBtn.addEventListener("click", getApi);
