6b74a0fa570e232e15d022014f343c11cbd9f3ed
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-20 22:08:20 -0600

    added pseudocode js

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+TODO: Event listener -- generate elements
+TODO: Link location name to coordinates
+TODO: Display one day
+TODO: Display days 2-6
+TODO: Save search to local storage
+TODO: Retain search as selectable button
+*/
